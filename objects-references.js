437b443807e249d8c5d7ce952be0639299d4fa18
mahaliahr <mahalia_hr@outlook.com>
2019-02-03 23:47:41 +0000

    adding expense tracker

--- objects-references.js
+++ objects-references.js
@@ -5,8 +5,28 @@ let myAccount = {
 }
 
 let addExpense = function (account, total) {
-    account.expense = account.expenses + total
+    account.expenses = account.expenses + total
 }
 
-addExpense(myAccount, 2.50)
-console.log(myAccount)
+let addIncome = function (account, total) {
+    account.income = account.income + total
+}
+
+let resetAccount = function (account) {
+    account.income = 0
+    account.expenses = 0
+}
+
+let getAccountSummary = function (account) {
+    return `Account for ${account.name} has $${account.income - account.expenses}. $${account.income} in income. $${account.expenses} in expenses.`
+}
+
+addIncome(myAccount, 5000)
+addExpense(myAccount, 300)
+addExpense(myAccount, 20)
+console.log(getAccountSummary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSummary(myAccount))
+
+
+
