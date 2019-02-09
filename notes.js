10faa51c46c6002b4eea3c8df8a1201cb0d22bc8
mahaliahr <mahalia_hr@outlook.com>
2019-02-09 16:14:41 +0000

    using find index on an array of objects

--- notes.js
+++ notes.js
@@ -1,11 +1,19 @@
-const notes = ['Note 1', 'Note 2', 'Note 3']
+const notes = [{
+    title: 'Work on my blog',
+    body: 'Regularly update and post content'
+}, {
+    title: 'Habits to work on',
+    body: 'Making sure to go to exercise class every week'
+}, {
+    title: 'Reading',
+    body: 'Aiming to read more. Daily reading.'
+}]
 
 /*console.log(notes.pop()) // removes last item in an array. returns the item 
 notes.push('My new note') // adds new item to the end
 
 console.log(notes.shift()) // removes first item in array
 console.log(notes.unshift('My first note')) // adds first item in the array
-*/
 
 // notes.splice(1, 1, 'This is the new second item') // two arguments, index first, then number of things you want to remove
 
@@ -15,6 +23,8 @@ notes.forEach(function (item, index) {
     console.log(index)
     console.log(item)
 })
+
+*/
 
 console.log(notes.length)
 console.log(notes)
@@ -30,3 +40,10 @@ for (let count = 0; count < notes.length; count ++) {
 }
 
 */
+
+const index = notes.findIndex(function (note, index) {
+    console.log(note)
+    return note.title === 'Habits to work on'
+})
+
+console.log(index)
