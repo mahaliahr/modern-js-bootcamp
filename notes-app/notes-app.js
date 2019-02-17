9cac0d8085e1692656062a3c53582b1ff84448cc
mahaliahr <mahalia_hr@outlook.com>
2019-02-17 00:40:31 +0000

    manipulating DOM with query selectors

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,1 +1,16 @@
-console.log('This is from a different file')
+// DOM - Document Object Model
+
+// Query and remove (singular)
+//const p = document.querySelector('p')
+
+// p.remove()
+
+// Query all and remove
+
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function (p){
+    p.textContent = '*********'
+    // p.remove()
+    // console.log(p.textContent)
+})
