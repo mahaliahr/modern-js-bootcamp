8c548cfb58e330d4c40a4d6c83255ccda3a590f6
mahaliahr <mahalia_hr@outlook.com>
2019-02-18 23:54:22 +0000

    adding element to the html with javascript

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,8 +1,34 @@
+const todo = [{
+    text: 'Laundry',
+    completed: true
+}, {
+    text: 'Go to the bank',
+    completed: false
+}, {
+    text: 'Go shopping',
+    completed: true 
+}, {
+    text: 'Tidy room',
+    completed: false
+}, {
+    text: 'Make phone calls',
+    completed: false
+}]
 
-const paragraphs = document.querySelectorAll('p')
+// You have 2 todos left - summary in para tag
 
-paragraphs.forEach(function (p){
-    if (p.textContent.includes('the')) {
-        p.remove()
-    }
+// add p for each todo above 
+
+const incompleteTodos = todo.filter(function (todo){
+    return !todo.completed
 })
+
+const todosLeft = document.createElement('h2')
+todosLeft.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(todosLeft)
+
+todo.forEach(function (todo){
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+})  
