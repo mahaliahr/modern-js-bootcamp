688a5a5f073ee035b95a409d1380d202de67d350
mahaliahr <mahalia_hr@outlook.com>
2019-02-24 23:40:30 +0000

    applying filtering to the todo app, and small edit to notes-app html file

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,24 +15,46 @@ const todo = [{
     completed: false
 }]
 
-const incompleteTodos = todo.filter(function (todo){
-    return !todo.completed
+const filters = {
+    searchText: ''
+}
+
+const renderTodos = function (todo, filters) {
+    const filteredTodos = todo.filter(function (todo){
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    }) 
+
+    const incompleteTodos = filteredTodos.filter(function (todo){
+        return !todo.completed
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    const todosLeft = document.createElement('h2')
+    todosLeft.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(todosLeft)
+
+    filteredTodos.forEach(function (todo){
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })  
+
+}
+
+renderTodos(todo, filters)  
+
+document.querySelector('#search-text').addEventListener('input', function(e) {
+    filters.searchText = e.target.value
+    renderTodos(todo, filters)
 })
 
-const todosLeft = document.createElement('h2')
-todosLeft.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(todosLeft)
-
-todo.forEach(function (todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
-})  
-
+// Listen for new todo creation
 document.querySelector('#add-todo').addEventListener('click', function (e) {
     console.log('Add new todo')
 })
 
+// Listen for todo text change
 document.querySelector('#type-new-todo').addEventListener('input', function(e) {
     console.log(e.target.value)
 })
