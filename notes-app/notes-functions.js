aa4fd88fd6e2bc627a4d3b55f0ef858b6f27aeec
mahaliahr <mahalia_hr@outlook.com>
2019-03-11 00:38:06 +0000

    redirecting user to seperate html page to edit notes

--- notes-app/notes-functions.js
+++ notes-app/notes-functions.js
@@ -27,7 +27,7 @@ const removeNote = function(id) {
 // Generate the DOM structure for a note
 const generateNoteDOM = function(note) {
     const noteEl = document.createElement('div')
-    const textEl = document.createElement('span')
+    const textEl = document.createElement('a')
     const button = document.createElement('button')
     
     // Setup the remove note button
@@ -45,6 +45,7 @@ const generateNoteDOM = function(note) {
     } else {
         textEl.textContent = 'Unamed note'
     }
+    textEl.setAttribute('href', `/edit.html#${note.id}`)
     noteEl.appendChild(textEl)
     return noteEl
 }
